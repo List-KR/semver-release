05c9ce514a558bfcec0a3a12df3bf1c05aba8af8
PiQuark6046 <piquark6046@proton.me>
2024-02-01 08:22:15 +0000

    2.1.1 (#6)
    * fix: semver versioning error
    
    * chore: update package version

--- sources/semver.ts
+++ sources/semver.ts
@@ -1,15 +1,22 @@
 import * as Semver from 'semver'
 import * as Luxon from 'luxon'
 
+function GetDaysAfterNewYear(Now: Luxon.DateTime<true>) {
+	const NewYear = Luxon.DateTime.utc(Now.year, 1, 1)
+	const Days = Now.diff(NewYear, 'days').days
+
+	return Math.floor(Days)
+}
+
 export function UpdateDateVersion(Version: string) {
 	const CurrentVersion = Semver.parse(Version)
 	var NewVersion = ''
 	var Now = Luxon.DateTime.utc()
 
 	// Check if the date is updated and change.
-	if ((CurrentVersion.major !== Now.year) || (CurrentVersion.minor.toString() !== `${Now.month}${Now.day}`)) {
+	if ((CurrentVersion.major !== Now.year) || (CurrentVersion.minor.toString() !== GetDaysAfterNewYear(Now).toString())) {
 		NewVersion += `${Now.year}.`
-		NewVersion += `${Now.month}${Now.day}.`
+		NewVersion += `${GetDaysAfterNewYear(Now)}.`
 		NewVersion += '0'
 	} else {
 		NewVersion = `${CurrentVersion.major}.${CurrentVersion.minor}.${CurrentVersion.patch + 1}`
