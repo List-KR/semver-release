25103f6ccecf186e5a55a662e65e69c93d335208
piquark6046 <piquark6046@proton.me>
2024-05-06 17:22:20 +0000

    add test for UpdateDateVersion with a past date

--- tests/semver.test.ts
+++ tests/semver.test.ts
@@ -7,6 +7,11 @@ test('UpdateDateVersion with current date', T => {
   if (UpdateDateVersion(`1${Now.year}.${GetDaysAfterNewYear(Now)}.0`) !== '1' + Now.year + '.' + GetDaysAfterNewYear(Now) + '.1') {
     T.fail()
   }
+  T.pass()
+})
+
+test('UpdateDateVersion with a past date', T => {
+  const Now = Luxon.DateTime.utc()
   if (UpdateDateVersion('2020.1.0') !== `1${Now.year}.${GetDaysAfterNewYear(Now)}.0`) {
     T.fail()
   }
